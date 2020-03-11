0b3e795715bab42017870135aa447a5f483a03b2
Chase Bowman <36805455+mortar-and-pestle@users.noreply.github.com>
2020-03-11 05:13:07 -0500

    Add server IP/Port Input

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 import threading
 import socket
+import time
 
 
 def connectToServer(destIP, port):
 
+    print("Attempting connection...")
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #clientSocket.connect(("192.168.1.119", 1234))
-    clientSocket.connect((socket.gethostname(), 1234))
+    clientSocket.connect((destIP, port))
+    print("You are now connected to the server!\n")
 
     return clientSocket
 
@@ -33,6 +35,8 @@ def recv(mySocket, headerLen):
 
 def send(mySocket, headerLen):
 
+    print("Chat away!\n")
+
     while True:
 
         msg = input()
@@ -43,19 +47,39 @@ def send(mySocket, headerLen):
         mySocket.send(bytes(msg, "utf-8"))
 
 
+def getDestIPandPort():
+
+    print("Welcome to Chatting with Friends!\n")
+    ip = input("Enter the IP address of the Server: ")
+    port = int(input("Enter the port number of the Server: "))
+
+    return ip, port
+
+
 def main():
 
+    #Each message passed to the socket contains a header describing the length of the message
+    #Since the string must be parsed by the server/clients, a predetermined number of characters are used to
+    #represent the length of the message
     headerLen = 10
-    mySocket = connectToServer("localHost", 1234)
 
+    #Get IP and Port of server
+    ip, port = getDestIPandPort()
+
+    #Setup connection to server by creating socket
+    mySocket = connectToServer(ip, port)
+
+    #Create two threads
+    #One thread prints any messages received from the server
+    #The other thread constantly asks for user input and sends it the server
     recvT = threading.Thread(target=recv, args=[mySocket, headerLen])
     sendT = threading.Thread(target=send, args=[mySocket, headerLen])
 
     recvT.start()
     sendT.start()
 
-    while True:
-        time.sleep(600)
+    recvT.join()
+    sendT.join()
 
 
 main()
